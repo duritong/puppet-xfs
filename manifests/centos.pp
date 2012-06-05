840a6672363121e477098e6e7c80cb79f9674f11
mh <mh@immerda.ch>
2012-06-05 22:24:18 +0200

    new style for 2.7

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,18 +1,18 @@
 class xfs::centos {
-    package{'xfsprogs':
-        ensure => present,
-    }
+  package{'xfsprogs':
+    ensure => present,
+  }
 
-    case $virtual {
-        xen0,xenu: {
-            package{'kmod-xfs-xen':
-                ensure => present,
-            }
-        }
-        default: {
-            package{'kmod-xfs':
-                ensure => present,
-            }
-        }
+  case $::virtual {
+    xen0,xenu: {
+      package{'kmod-xfs-xen':
+        ensure => present,
+      }
     }
+    default: {
+      package{'kmod-xfs':
+        ensure => present,
+      }
+    }
+  }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 # Manage xfs stuff
 
 class xfs {
-    case $operatingsystem {
-        centos: { include xfs::centos }
-        default: { fail("no xfs module yet defined for operatingsystem ${operatingsystem}") }
-    }
+  case $::operatingsystem {
+    centos: { include xfs::centos }
+    default: { fail("no xfs module yet defined for operatingsystem ${::operatingsystem}") }
+  }
 }
